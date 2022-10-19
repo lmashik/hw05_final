af90957b9a6a2a6667a1d39fce5a442314b1cb7d
Мария Лапикова <mashik_p@mail.ru>
2022-10-19 15:39:33 +0300

    add custom templates for errors

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -47,6 +47,8 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
     urlpatterns += static(
