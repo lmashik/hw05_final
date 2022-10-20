2104c9114d630af3f164e88c8a6c1fb53ba6ba3c
Мария Лапикова <mashik_p@mail.ru>
2022-10-20 16:03:38 +0300

    change constraints method; add checking of existing follow object

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -116,4 +116,9 @@ class Follow(models.Model):
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
-        unique_together = ('user', 'author',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='User is not author'
+            )
+        ]
